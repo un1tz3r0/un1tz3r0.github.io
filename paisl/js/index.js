a08b16a35bd2addad86aaf179f886b99bac951cb
Victor Condino <un1tz3r0@users.noreply.github.com>
2023-09-19 23:40:31 -0400

    fin

--- paisl/js/index.js
+++ paisl/js/index.js
@@ -386,14 +386,14 @@ function circleCircleIntersectionPoints(c1, c2) {
 			if((filfn == undefined) || filfn(this))
 			{
 				var subr = (this.r * Math.sin(Math.PI/num)) / (1.0 + Math.sin(Math.PI/num));
-				var subt = (Math.PI*2.0)/num;
+				var subt = (Math.PI)/num;
 				var centc = new Circle(this.x, this.y, (this.r - subr * 2) * this.s, this.a, this.g + 1, this, null, null);
 				var subcs = [];
 				var lastsubc = null;
 				for(let subi = 0; subi < num; subi++)
 				{
 					//var cursubc = new Circle(this.x, this.y - (this.r - subr), subr, this.a + (subi * subt) * (180.0/Math.PI), this.g + 1, this, [subi, num],  [this, centc]);
-					var cursubc = new Circle(this.x + Math.cos(subt * subi) * (this.r - subr), this.y + Math.sin(subt * subi) * (this.r - subr), subr, this.a + (subi * subt) * (360.0/Math.PI), this.g + 1, this, [subi, num],  [this, centc]);
+					var cursubc = new Circle(this.x + Math.cos(subt * subi) * (this.r - subr), this.y + Math.sin(subt * subi) * (this.r - subr), subr, this.a + (subi * subt), this.g + 1, this, [subi, num],  [this, centc]);
 					if(lastsubc != null) {
 						cursubc.addtouching(lastsubc);
 						lastsubc.addtouching(cursubc);
