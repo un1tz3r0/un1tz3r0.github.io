ba0543c36766ead7bb4255706b932c18d9f59b53
Victor Condino <un1tz3r0@github.com>
2023-10-31 12:43:43 -0400

    add channel and xfader classes, lets give it a go

--- audio/xfade.js
+++ audio/xfade.js
@@ -45,6 +45,110 @@ function fetchPlaylist(playlist) {
 }
 
 
+class Channel {
+	constructor(mixer) {
+		const actx = mixer.context;
+		this.mixer = mixer;
+		this.context = mixer.context;
+		this.gain = actx.createGain();
+		this.gain.connect(actx.destination);
+
+		this.source = actx.createBufferSource();
+		this.source.connect(this.gain);
+		//this.sources[i].loop = true;
+
+		this.param = actx.createParameterNode();
+		this.param.connect(this.gain.gain);
+		this.param.value = 0;
+
+		this.start_time = 0;
+		this.clip = null;
+		this.playing = false;
+		this.available = true;
+
+		this.source.addEventListener("ended", (evt)=>{
+			this.playing = false;
+			this.available = true;
+
+			this.mixer.scheduleNextClip();
+		}
+	}
+
+	get endTime() {
+		if(!this.available) {
+			return this.start_time + this.clip.duration;
+		} else {
+			return this.start_time;
+		}
+	}
+
+	scheduleClip(clip, start_time) {
+		this.clip = clip;
+		this.start_time = start_time;
+		this.available = false;
+		this.playing = true;
+		this.source.buffer = clip;
+		this.source.start(start_time);
+		// set up envelope
+		this.param.setValueAtTime(0, start_time);
+		this.param.linearRampToValueAtTime(1, start_time + this.mixer.overlap);
+		this.param.linearRampToValueAtTime(1, start_time + clip.duration - this.mixer.overlap);
+		this.param.linearRampToValueAtTime(0, start_time + clip.duration);
+	}
+};
+
+class XFadeRandomizer {
+	constructor(context, playlist, overlap) {
+		const actx = context;
+		this.context = context;
+		this.overlap = overlap;
+		this.playlist = playlist;
+		this.channels = [];
+				
+		for(let i of [0,1])
+		{
+			this.channels = new Channel(this);
+		}
+	}
+	
+	getRandomClip() {
+		const clips = this.playlist;
+		const keys = Object.keys(clips);
+		const idx = Math.floor(Math.random() * keys.length);
+		return clips[keys[idx]];
+	}
+
+	getAvailableChannel() {
+		for(let ch of this.channels) {
+			if(!ch.available) {
+				return ch;
+			}
+		}
+		return null;
+	}
+
+	scheduleNextClip() {
+		const next_clip = this.getRandomClip();
+		const next_start_time = -1;
+		for(let ch of this.channels) {
+			if(!ch.available) {
+				const ch_end_time = ch.endTime;
+				if(ch_end_time - this.overlap < next_start_time) {
+					next_start_time = ch_end_time - this.overlap;
+				}
+			}
+		}
+		const next_ch = this.getAvailableChannel();
+		if(next_ch == null) {
+			console.warn("No available channels!");
+			return false;
+		}
+		next_ch.scheduleClip(next_clip, next_start_time);
+		return true;
+	}
+}
+
+
 function startAudio()
 {
 	// create an AudioContext to use the web audio api
@@ -57,6 +161,14 @@ function startAudio()
 	{
 		// create our buffer sources and stuff
 		console.info("schedulePlayback() called...");
+
+		const xfade = new XFadeRandomizer(audioContext, clips, 2.0);
+		window._xfade = xfade;
+		while(xfade.scheduleNextClip())
+		{
+			console.info("Scheduled next clip...");
+		}
+		console.info("Done scheduling clips...");
 	}
 	
 	return fetchPlaylist(["clip1.mp3", "clip2.mp3", "clip3.mp3", "clip4.mp3"]).then((clips)=>{
@@ -64,11 +176,12 @@ function startAudio()
 		window._clips = clips;
 		audioclips = clips;
 		
-		schedulePlayback();
+		schedulePlayback(clips);
 	});
 }
 
-document.addEventListener("domcontentloaded", (evt)=>{
+function setupAudio()
+{
 	const pausedOverlay = document.createElement("div");
 	pausedOverlay.setAttribute("class", "show");
 	pausedOverlay.style.transition = "all 1s ease-in-out";
@@ -97,14 +210,12 @@ document.addEventListener("domcontentloaded", (evt)=>{
 			startAudio().then(()=>{
 				pausedOverlay.innerHTML = "&lt; Loaded! &gt;";
 				console.info("Audio running and loaded, calling scheduleAudio()");
-				scheduleAudio();
 			});
 			pausedOverlay.innerHTML = "&lt; Loading... &gt;";
-			pausedOverlay.style.top = "-100vh";
 		} else {
 			pausedOverlay.innerHTML = "&lt; Click to play... &gt;";
 			pausedOverlay.style.top = "0vh";
 		}
 		pausedOverlay.classList.toggle("show");
 	});
-});
+}
