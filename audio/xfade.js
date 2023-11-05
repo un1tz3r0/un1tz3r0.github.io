36af95f45955e1b80072ab0b05bc15a26de1c6d3
Victor Condino <un1tz3r0@github.com>
2023-11-05 10:40:34 -0500

    mv icon setup and reference funcs into own module (/icons.js) from audio/xfade.js small cleanups to index.html, getting closer

--- audio/xfade.js
+++ audio/xfade.js
@@ -199,11 +199,10 @@ function startAudio()
 	});
 }
 
-var iconsSetup = false;
 var iconsDefsElem = null;
 function setupIcons()
 {
-	if(iconsSetup != false)
+	if(iconsDefsElem != null)
 	{
 		return iconsDefsElem;
 	}
@@ -222,15 +221,41 @@ function setupIcons()
 		<symbol id="av_volume_up_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M3 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L7 9H4c-.55 0-1 .45-1 1zm13.5 2c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 4.45v.2c0 .38.25.71.6.85C17.18 6.53 19 9.06 19 12s-1.82 5.47-4.4 6.5c-.36.14-.6.47-.6.85v.2c0 .63.63 1.07 1.21.85C18.6 19.11 21 15.84 21 12s-2.4-7.11-5.79-8.4c-.58-.23-1.21.22-1.21.85z"/></symbol>
 ```;
 		document.body.appendChild(iconsvgelem);
-		iconsSetup = true;
-		return iconsvgelem;
+		iconsDefsElem = icondefselem;
+		return iconssvgelem;
 	}
 }
+
+function createIconElement(name, size=24)
+{
+	if(iconsDefsElem == null)
+	{
+		setupIcons();
+	}
+	
+	/*
+	<svg viewbox="0 0 24 24" width="96px" height="96px" xmlns="http://www.w3.org/2000/svg">
+	<use href="#av_volume_off_materialiconsround" x="0" y="0"/>
+	</svg>
+	*/
+	
+	const iconsvgelem = document.createElementNS("svg", "http://www.w3.org/2000/svg");
+	iconsvgelem.setAttribute("viewbox", "0 0 24 24");
+	iconsvgelem.setAttribute("width", `${size}px`);
+	iconsvgelem.setAttribute("height", `${size}px`);
+	const iconuseelem = document.createElement("use");
+	iconsvgelem.appendChild(iconuseelem);
+	iconuseelem.setAttribute("href", `#${name}`);
+	iconuseelem.setAttribute("x", "0");
+	iconuseelem.setAttribute("y", "0");
+	return iconsvgelem;
+}
+
 
 function setupAudio()
 {
 	const pausedOverlay = document.createElement("div");
-	pausedOverlay.setAttribute("class", "show");
+	pausedOverlay.setAttribute("class", "pausedOverlay show");
 	pausedOverlay.style.transition = "all 1s ease-in-out";
 	pausedOverlay.style.position = "fixed";
 	pausedOverlay.style.margin = "0";
@@ -246,8 +271,9 @@ function setupAudio()
 	pausedOverlay.style.transition = "all 1s ease-in";
 	pausedOverlay.style.overflow = "hidden";
 	pausedOverlay.style.backgroundColor = "#bbb4";
+	pausedOverlay.style.transform = "translateY(0vh)";
 	pausedOverlay.innerHTML = "&lt; Click to play... &gt;";
-
+	
 	//const styleElem = document.createElement("style");
 
 	document.body.appendChild(pausedOverlay);
@@ -260,9 +286,16 @@ function setupAudio()
 				console.info("Audio running and loaded, calling scheduleAudio()");
 				pausedOverlay.style.transform = "translateY(-100vh)";
 			});
-			pausedOverlay.innerHTML = "&lt; Loading... &gt;";
+			pausedOverlay.innerHTML = \
+			'<svg viewbox="0 0 24 24" width="96px" height="96px" xmlns="http://www.w3.org/2000/svg">' + \
+	'<use href="#av_volume_down_materialiconsround" x="0" y="0"/>' + \
+	'</svg>';
+
 		} else {
-			pausedOverlay.innerHTML = "&lt; Click to play... &gt;";
+			pausedOverlay.innerHTML = \			
+			'<svg viewbox="0 0 24 24" width="96px" height="96px" xmlns="http://www.w3.org/2000/svg">' + \
+	'<use href="#av_volume_off_materialiconsround" x="0" y="0"/>' + \
+	'</svg>';
 			pausedOverlay.style.transform = "translateY(0vh)";
 		}
 		pausedOverlay.classList.toggle("show");
