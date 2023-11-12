935df43a589d94d49f2be5683137a9ad5617a303
Victor Condino <un1tz3r0@gmail.com>
2023-11-12 08:15:22 -0500

    add rt display to audio demo + cleaner app. wip

--- audio/xfade.js
+++ audio/xfade.js
@@ -16,15 +16,6 @@ async function fetchAudio(context, name) {
   }
 }
 
-/*
-function fetchnamedclip(clipname, dest) {
-	return fetchAudio(clipname).then((buf) => {
-		console.info(`Loaded clip ${clipname}.`
-		clips[clipname] = buf;
-	});
-}
-*/
-
 function fetchPlaylist(context, playlist) {
 	var fetchedclips = {};
 	function fetchNext(playlist, fetchedclips) {
@@ -35,6 +26,7 @@ function fetchPlaylist(context, playlist) {
 				var clipname = playlist[0];
 				var remainingclips = playlist.slice(1);
 				return fetchAudio(context, clipname).then((clip)=>{
+					clip.filename = clipname;
 					fetchedclips[clipname] = clip;
 					ok(await fetchNext(remainingclips, fetchedclips));
 				});
@@ -43,7 +35,6 @@ function fetchPlaylist(context, playlist) {
 	}
 	return fetchNext(playlist, fetchedclips);
 }
-
 
 class Channel {
 	constructor(mixer) {
@@ -82,25 +73,50 @@ class Channel {
 
 			chan.mixer.scheduleNextClip();
 		});
-		//this.sources[i].loop = true;
 	}
-	
+
 	get endTime() {
 		if(!this.available) {
-			return this.start_time + this.clip.duration;
+			return this.start_time + ((this.clip != null) ? this.clip.duration : 0.0);
 		} else {
 			return 0;
 		}
 	}
+
+	get clipName() {
+		if(this.clip != null)
+			return this.clip.filename;
+		else
+			return "null";
+	}
 	
+	get clipTime() {
+		const t = this.context.currentTime - this.start_time;
+		if(t < 0)
+			return 0;
+		return t;
+	}
+
+	get channelGain() {
+		return this.param.value;
+	}
+
+	get channelState() {
+		if(this.clipTime <= 0) {
+			return "pending";
+		} else {
+			if(this.available) {
+				return "stopped";
+			} else {
+				return "playing";
+			}
+		}
+	}
+
 	scheduleClip(clip, start_time) {
 		console.info(`Scheduling clip ${clip} at time ${start_time}...`);
-		/*if(this.source.buffer != null) {
-			this.source.buffer = null;
-		}
-		this.source*/
 		this.initSource();
-		
+
 		this.clip = clip;
 		this.start_time = start_time;
 		this.available = false;
@@ -123,13 +139,13 @@ class XFadeRandomizer {
 		this.overlap = overlap;
 		this.playlist = playlist;
 		this.channels = [];
-				
+
 		for(let i of [0,1])
 		{
 			this.channels[i] = new Channel(this);
 		}
 	}
-	
+
 	getRandomClip() {
 		const clips = this.playlist;
 		const keys = Object.keys(clips);
@@ -167,7 +183,27 @@ class XFadeRandomizer {
 	}
 }
 
+// update the visual feedback of the channels' playback states
+function startAnimate(xfade) {
+	var animateHandle = none;
 
+	function animate(frametime)
+	{
+		for(let i of [0,1]) {
+			document.querySelector(`#channel${i+1}_time`).value = String(xfade.channels[i].clipTime);
+			document.querySelector(`#channel${i+1}_state`).value = String(xfade.channels[i].channelState);
+			document.querySelector(`#channel${i+1}_clip`).value = String(xfade.channels[i].clipName);
+			document.querySelector(`#channel${i+1}_gain`).value = String(xfade.channels[i].channelGain);
+		}
+		animateHandle = window.requestAnimationFrame(animate);
+	}
+
+	animateHandle = window.requestAnimationFrame(animate);
+}
+
+// create audiocontext on user input (to work with anti-autoplay restriction)
+// also loads the clips in the playlist, and then creates the xfade mixer and
+// schedules the clips and envelopes, finally kicks of the update animation loop
 function startAudio()
 {
 	// create an AudioContext to use the web audio api
@@ -175,7 +211,9 @@ function startAudio()
 	const audioContext = new AudioContext();
 	window._audioContext = audioContext;
 	audiocontext = audioContext;
-	
+
+	// this will create and initialize the xfader with two channels and start them
+	// ping-pong playing once the clips are all ready
 	function schedulePlayback(clips)
 	{
 		// create our buffer sources and stuff
@@ -188,17 +226,23 @@ function startAudio()
 			console.info("Scheduled clip...");
 		}
 		console.info("Done scheduling clips...");
+		return xfade;
 	}
-	
+
+	// this loads the clips asynchronously and then calls the above 
+	// function when they are ready
 	return fetchPlaylist(audioContext, ["clip1.mp3", "clip2.mp3", "clip3.mp3", "clip4.mp3"]).then((clips)=>{
 		console.info("Done loading samples...", clips);
 		window._clips = clips;
 		audioclips = clips;
-		
-		schedulePlayback(clips);
+
+		const xfade = schedulePlayback(clips);
+		startAnimation(xfade);
 	});
 }
 
+// misc non-audio-related icon graphics as svg symbols injected into the document
+// so we may use them in the UI elements we construct for the xfader etc.
 var iconsDefsElem = null;
 function setupIcons()
 {
@@ -212,20 +256,19 @@ function setupIcons()
 		iconsvgelem.style.display = "none";
 		const icondefselem = document.createElement("defs");
 		iconsvgelem.appendChild(icondefselem);
-		icondefselem.innerHTML = ```<symbol id="av_volume_off_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M3.63 3.63c-.39.39-.39 1.02 0 1.41L7.29 8.7 7 9H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71v-4.17l4.18 4.18c-.49.37-1.02.68-1.6.91-.36.15-.58.53-.58.92 0 .72.73 1.18 1.39.91.8-.33 1.55-.77 2.22-1.31l1.34 1.34c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L5.05 3.63c-.39-.39-1.02-.39-1.42 0zM19 12c0 .82-.15 1.61-.41 2.34l1.53 1.53c.56-1.17.88-2.48.88-3.87 0-3.83-2.4-7.11-5.78-8.4-.59-.23-1.22.23-1.22.86v.19c0 .38.25.71.61.85C17.18 6.54 19 9.06 19 12zm-8.71-6.29l-.17.17L12 7.76V6.41c0-.89-1.08-1.33-1.71-.7zM16.5 12c0-1.77-1.02-3.29-2.5-4.03v1.79l2.48 2.48c.01-.08.02-.16.02-.24z"/></symbol>
-
-		<symbol id="av_volume_down_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L9 9H6c-.55 0-1 .45-1 1z"/></symbol>
-
-		<symbol id="av_volume_mute_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M7 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L11 9H8c-.55 0-1 .45-1 1z"/></symbol>
-
-		<symbol id="av_volume_up_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M3 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L7 9H4c-.55 0-1 .45-1 1zm13.5 2c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 4.45v.2c0 .38.25.71.6.85C17.18 6.53 19 9.06 19 12s-1.82 5.47-4.4 6.5c-.36.14-.6.47-.6.85v.2c0 .63.63 1.07 1.21.85C18.6 19.11 21 15.84 21 12s-2.4-7.11-5.79-8.4c-.58-.23-1.21.22-1.21.85z"/></symbol>
-```;
+		icondefselem.innerHTML = ```
+			<symbol id="av_volume_off_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M3.63 3.63c-.39.39-.39 1.02 0 1.41L7.29 8.7 7 9H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71v-4.17l4.18 4.18c-.49.37-1.02.68-1.6.91-.36.15-.58.53-.58.92 0 .72.73 1.18 1.39.91.8-.33 1.55-.77 2.22-1.31l1.34 1.34c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L5.05 3.63c-.39-.39-1.02-.39-1.42 0zM19 12c0 .82-.15 1.61-.41 2.34l1.53 1.53c.56-1.17.88-2.48.88-3.87 0-3.83-2.4-7.11-5.78-8.4-.59-.23-1.22.23-1.22.86v.19c0 .38.25.71.61.85C17.18 6.54 19 9.06 19 12zm-8.71-6.29l-.17.17L12 7.76V6.41c0-.89-1.08-1.33-1.71-.7zM16.5 12c0-1.77-1.02-3.29-2.5-4.03v1.79l2.48 2.48c.01-.08.02-.16.02-.24z"/></symbol>
+			<symbol id="av_volume_down_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L9 9H6c-.55 0-1 .45-1 1z"/></symbol>
+			<symbol id="av_volume_mute_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M7 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L11 9H8c-.55 0-1 .45-1 1z"/></symbol>
+			<symbol id="av_volume_up_materialiconsround" height="24" viewBox="0 0 24 24" width="24"><path d="M3 10v4c0 .55.45 1 1 1h3l3.29 3.29c.63.63 1.71.18 1.71-.71V6.41c0-.89-1.08-1.34-1.71-.71L7 9H4c-.55 0-1 .45-1 1zm13.5 2c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 4.45v.2c0 .38.25.71.6.85C17.18 6.53 19 9.06 19 12s-1.82 5.47-4.4 6.5c-.36.14-.6.47-.6.85v.2c0 .63.63 1.07 1.21.85C18.6 19.11 21 15.84 21 12s-2.4-7.11-5.79-8.4c-.58-.23-1.21.22-1.21.85z"/></symbol>
+		```;
 		document.body.appendChild(iconsvgelem);
 		iconsDefsElem = icondefselem;
 		return iconssvgelem;
 	}
 }
 
+// create a DOM element which references one the svg symbol icons defined by the above function
 function createIconElement(name, size=24)
 {
 	if(iconsDefsElem == null)
@@ -251,7 +294,7 @@ function createIconElement(name, size=24)
 	return iconsvgelem;
 }
 
-
+// 
 function setupAudio()
 {
 	const pausedOverlay = document.createElement("div");
